ff5ba886492cb551f434bef0216b15f509cf464e
Mounir Nessim <mmnessim@gmail.com>
2026-01-13 08:04:04 -0500

    Prevent reinitialization of koin on iOS when rotating screen. Fixed crashing on rotate

--- composeApp/src/iosMain/kotlin/com/mnessim/researchtrackerkmp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/mnessim/researchtrackerkmp/MainViewController.kt
@@ -5,6 +5,7 @@ import com.mnessim.researchtrackerkmp.domain.data.DBFactory
 import com.mnessim.researchtrackerkmp.domain.services.WorkService
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import platform.Foundation.NSNumber
@@ -13,14 +14,23 @@ import platform.UserNotifications.UNAuthorizationOptionBadge
 import platform.UserNotifications.UNAuthorizationOptionSound
 import platform.UserNotifications.UNUserNotificationCenter
 
+private var koinApp: KoinApplication? = null
+
+fun initKoinIfNeeded(): KoinApplication {
+    if (koinApp == null) {
+        val iosPlatformModule = module {
+            single<DBFactory> { DBFactory() }
+            single<WorkService> { WorkService() }
+        }
+        koinApp = startKoin {
+            modules(commonModules + iosPlatformModule)
+        }
+    }
+    return koinApp!!
+}
+
 fun MainViewController(detailsId: NSNumber? = null) = ComposeUIViewController {
-    val iosPlatformModule = module {
-        single<DBFactory> { DBFactory() }
-        single<WorkService> { WorkService() }
-    }
-    val koinApp = startKoin {
-        modules(commonModules + iosPlatformModule)
-    }
+    val koinApp = initKoinIfNeeded()
 
     MainScope().launch {
         requestNotificationPermissions()
